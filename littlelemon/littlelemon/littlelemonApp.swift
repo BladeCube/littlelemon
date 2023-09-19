b79cec4556b627a79f1703b375985ffc15861670
BladeCube <63940229+BladeCube@users.noreply.github.com>
2023-09-19 00:12:14 -0400

    init

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Mark Chen on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
